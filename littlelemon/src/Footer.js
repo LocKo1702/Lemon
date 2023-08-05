f7026bf41ab2003d20492f8b597c24af699c2906
Kourtney <Locko1702@gmail.com>
2023-08-05 10:44:57 +0200

    Semantic Structure, Meta Tags, OGP

---
+++ littlelemon/src/Footer.js
@@ -0,0 +1,33 @@
+function Footer() {
+    return (
+        <footer>
+            <div class="foot">
+                <img src="logo_larger.png" alt="Large Logo"></img>
+            </div>
+            <div class="foot2">
+                <ul>
+                    <li style="font-size:300%"><bold>Navigation</bold></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Menu</a></li>
+                    <li><a href="#">Reservations</a></li>
+                    <li><a href="#">Order Online</a></li>
+                    <li><a href="#">Login</a></li>
+                </ul>
+            </div>
+            <div class="foot3">
+                <p style="font-size:300%"><bold>Contact</bold></p>
+                <p>333 Lucky Ave<br/>
+                   Chicago, IL<br/>
+                   90210<br/>
+                </p>
+            </div>
+            <div class="foot4">
+                <li style="font-size:300%"><bold>Social Media</bold></li>
+                <li><a href="instagram.com">Instagram</a></li>
+                <li><a href="facebook.com">Facebook</a></li>
+                <li><a href="google.com">Google</a></li>
+            </div>
+        </footer>
+    )
+}
